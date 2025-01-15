319fd65144be59a101b704aa98d725f812b41459
Juho Teperi <juho.teperi@iki.fi>
2025-01-15 11:31:02 +0200

    Run test case with set timezone
    Parsing strange non-date values to dates seems to depend on the JVM
    timezone

--- test/loiste/core_test.clj
+++ test/loiste/core_test.clj
@@ -2,7 +2,8 @@
   (:require [clojure.test :refer [deftest testing is]]
             [loiste.core :as l]
             [clojure.java.io :as io])
-  (:import [java.io File]))
+  (:import [java.io File]
+           [java.util TimeZone]))
 
 (def test-spec
   [[:A]
@@ -63,50 +64,55 @@
 (deftest write-new-file-test
   ;; Keep the file around after tests for testing opening,
   ;; but remove on the start.
-  (let [file (io/file "test-out" "new-file-test.xslx")]
-    (io/make-parents file)
-    (doseq [wb* [(l/workbook)
-                 (l/streaming-workbook)]]
-      (.delete file)
-      (with-open [wb wb*]
-        (let [sheet (l/sheet wb)]
-          (println "Test file " (.getPath file))
-          (l/write-rows!
-            wb
-            sheet
-            {:styles {:date {:data-format {:type :date :pattern "dd.MM.yyyy HH.MM" :locale "fi"}}
-                      :currency {:data-format {:type :custom :format-str "#0\\,00 €"}}
-                      :header {:font {:bold true}
-                               :border-bottom :thick
-                               :border-right :thin
-                               :fill-pattern :solid
-                               :foreground-color (l/color 200 200 200)}
-                      :wrapping-text {:wrap true}}
-             :columns [{:width (l/column-width 10)}
-                       {:width (l/column-width 15) :style :date}
-                       {:width (l/column-width 10) :style :currency}]}
-            [[{:style :header :value "Stuff"}
-              {:style :header :value "Date"}
-              {:style :header :value "Money"}]
-             {:height 100
-              :values [{:style :wrapping-text
-                        :value "This is a long text that should wrap"}
-                       #inst "2016-03-09T14:05:00"]}
-             ["Bar" #inst "2016-03-09T14:05:00" 15]
-             ["Qux" 100000 100]
-             ["Integer" (count '(1 2 3)) 10000]
-             ["Keyword" :foo nil]])
+  (let [old-tz (TimeZone/getDefault)
+        file (io/file "test-out" "new-file-test.xslx")]
+    (try
+      (TimeZone/setDefault (TimeZone/getTimeZone "Europe/Helsinki"))
+      (io/make-parents file)
+      (doseq [wb* [(l/workbook)
+                   (l/streaming-workbook)]]
+        (.delete file)
+        (with-open [wb wb*]
+          (let [sheet (l/sheet wb)]
+            (println "Test file " (.getPath file))
+            (l/write-rows!
+              wb
+              sheet
+              {:styles {:date {:data-format {:type :date :pattern "dd.MM.yyyy HH.MM" :locale "fi"}}
+                        :currency {:data-format {:type :custom :format-str "#0\\,00 €"}}
+                        :header {:font {:bold true}
+                                 :border-bottom :thick
+                                 :border-right :thin
+                                 :fill-pattern :solid
+                                 :foreground-color (l/color 200 200 200)}
+                        :wrapping-text {:wrap true}}
+               :columns [{:width (l/column-width 10)}
+                         {:width (l/column-width 15) :style :date}
+                         {:width (l/column-width 10) :style :currency}]}
+              [[{:style :header :value "Stuff"}
+                {:style :header :value "Date"}
+                {:style :header :value "Money"}]
+               {:height 100
+                :values [{:style :wrapping-text
+                          :value "This is a long text that should wrap"}
+                         #inst "2016-03-09T14:05:00"]}
+               ["Bar" #inst "2016-03-09T14:05:00" 15]
+               ["Qux" 100000 100]
+               ["Integer" (count '(1 2 3)) 10000]
+               ["Keyword" :foo nil]])
 
-          (l/to-file! file wb))))
+            (l/to-file! file wb))))
 
-    (with-open [wb (l/workbook file)]
-      (let [read-sheet (l/sheet wb 0)]
-        (is (= [{:Stuff "This is a long text that should wrap" :Date #inst "2016-03-09T14:05:00" :Money nil}
-                {:Stuff "Bar" :Date #inst "2016-03-09T14:05:00" :Money 15.0}
-                {:Stuff "Qux" :Date #inst "2173-10-13T21:00:00.000-00:00" :Money 100.0}
-                {:Stuff "Integer" :Date #inst "1900-01-02T22:20:11.000-00:00" :Money 10000.0}
-                {:Stuff "Keyword" :Date "foo" :Money nil}]
-               (l/read-sheet read-sheet)))))))
+      (with-open [wb (l/workbook file)]
+        (let [read-sheet (l/sheet wb 0)]
+          (is (= [{:Stuff "This is a long text that should wrap" :Date #inst "2016-03-09T14:05:00" :Money nil}
+                  {:Stuff "Bar" :Date #inst "2016-03-09T14:05:00" :Money 15.0}
+                  {:Stuff "Qux" :Date #inst "2173-10-13T21:00:00.000-00:00" :Money 100.0}
+                  {:Stuff "Integer" :Date #inst "1900-01-02T22:20:11.000-00:00" :Money 10000.0}
+                  {:Stuff "Keyword" :Date "foo" :Money nil}]
+                 (l/read-sheet read-sheet)))))
+      (finally
+        (TimeZone/setDefault old-tz)))))
 
 (deftest color-test
   (is (= (unchecked-byte 200)
